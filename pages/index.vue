8de77a1ce43c1f861c568e305d7e4508ef972047
Chaloulou75 <c.jeandey@gmail.com>
2020-10-15 13:38:19 -0300

    letras big screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen bg-background-primary" :class="theme">
+    <div class="min-h-screen overflow-y-hidden bg-background-primary" :class="theme">
       <Navigation />
       <div class="relative">
 
@@ -25,10 +25,10 @@
       <template v-else-if="foto === 'Amarillo'">
         <div class="flex items-center justify-between p-32 space-between bg-background-primary" >
           <div class="relative w-1/2 h-64 max-w-lg overflow-hidden rounded-lg shadow-2xl" >
-            <img class="absolute inset-0 object-cover w-full h-full" src="~assets/img/portadas/amarillo/una.jpg" alt="amarillouna" />
+            <img class="absolute inset-0 object-cover w-full h-full " src="~assets/img/portadas/amarillo/una.jpg" alt="amarillouna" />
             <div class="absolute inset-0 bg-gray-900 bg-opacity-25"></div>
             <div class="relative flex items-center justify-center h-full">
-              <h1 class="text-3xl tracking-wider text-teal-100"></h1>
+              <h1 class="text-3xl tracking-wider text-teal-100 "></h1>
             </div>
           </div>
           <div class="relative w-1/2 h-64 max-w-lg overflow-hidden rounded-lg shadow-2xl">
@@ -55,20 +55,20 @@
         <button class="absolute top-0 right-0 px-2 py-3 mr-6 text-copy-primary focus:outline-none" @click="toggle">Start Me</button>
 
         <div class="absolute inset-y-0 right-0 mt-16 mr-12">
-          <div class="fixed space-y-3">
-            <div class="w-6 h-6 bg-blue-500 border border-gray-500"
+          <div class="fixed space-y-4">
+            <div class="w-6 h-6 transition duration-300 transform bg-blue-500 border border-gray-500 rounded shadow-lg hover:scale-110 hover:-rotate-45 "
             @click="foto = 'Azul'"
             @mouseover="foto = 'Azul'"
             >
             </div>
 
-            <div class="w-6 h-6 border border-gray-500 bg-yellowjuli"
+            <div class="w-6 h-6 transition duration-300 transform border border-gray-500 rounded shadow-lg bg-yellowjuli hover:scale-110 hover:-rotate-45"
             @click="foto = 'Amarillo'"
             @mouseover="foto = 'Amarillo'"
             >
             </div>
 
-            <div class="w-6 h-6 border border-gray-500 bg-redjuli"
+            <div class="w-6 h-6 transition duration-300 transform border border-gray-500 rounded shadow-lg bg-redjuli hover:scale-110 hover:-rotate-45"
             @click="foto = 'Rojo'"
             @mouseover="foto = 'Rojo'"
             >
@@ -78,7 +78,7 @@
 
         <!-- las letras -->
         <div>
-          <div v-bind:class="{ 'anim_letter-p' : show}" class=" letter-p letterBig text-copy-primary">
+          <div v-bind:class="{ 'anim_letter-p' : show}" class="letter-p letterBig text-copy-primary">
              <nuxt-link to="/proyectos">P</nuxt-link>
           </div>
           <div v-bind:class="{ 'anim_letter-e' : show}" class="transform rotate-90 letter-e letterBig text-copy-primary">
@@ -132,20 +132,21 @@ export default {
 </script>
 
 <style scoped>
+
 .letter-p {
   top: 255px;
   left: 150px;
   transform-origin: left;
 
   @media (min-width: 767px) {
-    top: 5px;
-    left: 580px;
+    top: -5%;
+    left: 45%;
     transform-origin: left;
   }
 }
 
 .anim_letter-p {
-  animation: letterP 5.4s 0.5s forwards 1 ease-in-out normal running;
+  animation: letterP 6.4s 0.5s forwards 1 ease-in-out normal running;
   &:hover {
     animation-play-state: paused;
   }
@@ -153,15 +154,13 @@ export default {
 
 @keyframes letterP {
   0% {
-    transform:  translate(0px, 0px)  rotate(0deg);
-    opacity: 1;
+    transform:  translate()  rotate(0deg);
   }
-  52% {
-    transform:  translate(-120px, 50px)  rotate(360deg) ;
+  40%, 60% {
+    transform:  translate(-380%, 100%) rotate(360deg) ;
   }
   100% {
-    transform:  translate(225px, 15px)  rotate(0deg);
-    opacity: 1;
+    transform:  translate(200%, 15%) rotate(0deg);
   }
 }
 
@@ -171,14 +170,14 @@ export default {
   transform-origin: left;
 
   @media (min-width: 767px) {
-    top: 12px;
-    left: 500px;
+    top: 0%;
+    left: 34%;
     transform-origin: left;
   }
 }
 
 .anim_letter-e {
-  animation: letterE 4.6s 0.5s forwards 1 ease-in-out normal running;
+  animation: letterE 5.6s 0.5s forwards 1 ease-in-out normal running;
   &:hover {
     animation-play-state: paused;
   }
@@ -186,13 +185,13 @@ export default {
 
 @keyframes letterE {
   0% {
-    transform:  translate(0px,0px)  rotate(90deg);
+    transform:  translate()  rotate(90deg);
   }
-  45% {
-    transform:  translate(120px,-81px)  rotate(45deg) ;
+  40%, 60% {
+    transform:  translate(10%, 75%) rotate(-90deg);
   }
   100% {
-    transform:  translate(268px,-138px)  rotate(90deg);
+    transform:  translate(305%, -50%)  rotate(90deg);
   }
 }
 
@@ -202,14 +201,14 @@ export default {
   transform-origin: left;
 
   @media (min-width: 767px) {
-    top: 120px;
-    left: 280px;
+    top: 25%;
+    left: 17%;
     transform-origin: left;
   }
 }
 
 .anim_letter-r {
-  animation: letterR 5.1s 0.5s forwards 1 ease-in-out normal running;
+  animation: letterR 6.1s 0.5s forwards 1 ease-in-out normal running;
   &:hover {
     animation-play-state: paused;
   }
@@ -217,13 +216,13 @@ export default {
 
 @keyframes letterR {
   0% {
-    transform:  translate(0px, 0px)  rotate(0deg);
+    transform:  translate()  rotate(0deg);
   }
-  41% {
-    transform:  translate(120px,-81px)  rotate(360deg) ;
+  40%, 60%  {
+    transform:  translate(300%, -37%) rotate(360deg);
   }
   100% {
-    transform:  translate(90px,50px)  rotate(0deg);
+    transform:  translate(55%, 22%) rotate(0deg);
   }
 }
 
@@ -233,14 +232,14 @@ export default {
   transform-origin: left;
 
   @media (min-width: 767px) {
-    top: 180px;
-    left: 500px;
+    top: 35%;
+    left: 34%;
     transform-origin: left;
   }
 }
 
 .anim_letter-s {
-  animation: letterS 4.3s 0.5s forwards 1 ease-in-out normal running;
+  animation: letterS 5.3s 0.5s forwards 1 ease-in-out normal running;
   &:hover {
     animation-play-state: paused;
   }
@@ -248,13 +247,13 @@ export default {
 
 @keyframes letterS {
   0% {
-    transform:  translate(0px,0px)  rotate(90deg);
+    transform:  translate()  rotate(90deg);
   }
-  55% {
-    transform:  translate(180px,-81px)  rotate(360deg) ;
+  40%, 60% {
+    transform:  translate(350%, 2%)  rotate(-90deg) ;
   }
   100% {
-    transform:  translate(115px,-70px)  rotate(90deg);
+    transform:  translate(110%, -25%) rotate(90deg);
   }
 }
 
@@ -264,13 +263,13 @@ export default {
   transform-origin: left;
 
   @media (min-width: 767px) {
-    top: 280px;
-    left: 580px;
+    top: 63%;
+    left: 45%;
     transform-origin: left;
   }
 }
 .anim_letter-o {
-  animation: letterO 5s 0.5s forwards 1 ease-in-out normal running;
+  animation: letterO 6s 0.5s forwards 1 ease-in-out normal running;
   &:hover {
     animation-play-state: paused;
   }
@@ -278,13 +277,13 @@ export default {
 
 @keyframes letterO {
   0% {
-    transform:  translate(0px, 0px)  rotate(0deg);
+    transform:  translate()  rotate(0deg);
   }
-  52% {
-    transform:  translate(320px, -421px)  rotate(1080deg);
+  40%, 60%  {
+    transform:  translate(180%, -115%) rotate(720deg);
   }
   100% {
-    transform:  translate(-40px,0px)  rotate(0deg);
+    transform:  translate(-90%, -10%)  rotate(0deg);
   }
 }
 
@@ -294,13 +293,13 @@ export default {
   transform-origin: left;
 
   @media (min-width: 767px) {
-    top: 50px;
-    left: 900px;
+    top: 5%;
+    left: 65%;
     transform-origin: left;
   }
 }
 .anim_letter-n {
-  animation: letterN 4.9s 0.5s forwards 1 ease-in-out normal running;
+  animation: letterN 5.9s 0.5s forwards 1 ease-in-out normal running;
   &:hover {
     animation-play-state: paused;
   }
@@ -308,13 +307,13 @@ export default {
 
 @keyframes letterN {
   0% {
-    transform:  translate(0px, 0px)  rotate(0deg);
+    transform:  translate()  rotate(0deg);
   }
-  55% {
-    transform:  translate(170px,-81px)  rotate(120deg) ;
+  40%, 60%  {
+    transform:  translate(-125%, -38%)  rotate(360deg) ;
   }
   100% {
-    transform:  translate(-200px, 120px)  rotate(0deg);
+    transform:  translate(-150%, 60%)  rotate(0deg);
   }
 }
 
@@ -324,13 +323,13 @@ export default {
   transform-origin: left;
 
   @media (min-width: 767px) {
-    top: 190px;
-    left: 900px;
+    top: 41%;
+    left: 65%;
     transform-origin: left;
   }
 }
 .anim_letter-a {
-  animation: letterA 5.6s 0.5s forwards 1 ease-in-out normal running;
+  animation: letterA 6.2s 0.5s forwards 1 ease-in-out normal running;
   &:hover {
     animation-play-state: paused;
   }
@@ -338,23 +337,23 @@ export default {
 
 @keyframes letterA {
   0% {
-    transform:  translate(0px, 0px)  rotate(0deg);
+    transform:  translate()  rotate(0deg);
   }
-  62% {
-    transform:  translate(120px,-81px) rotate(190deg);
+  40%, 60%  {
+    transform:  translate(-300%, 15%) rotate(-360deg);
   }
   100% {
-    transform:  translate(200px, -20px) rotate(0deg);
+    transform:  translate(170%, -9%) rotate(0deg);
   }
 }
 
 @media (max-width: 767px) {
   @keyframes letterP {
     0% {
-      transform:  translate(0px, 0px)  rotate(0deg);
+      transform:  translate()  rotate(0deg);
       opacity: 1;
     }
-    52% {
+    40%, 60% {
       transform:  translate(-30px, 10px)  rotate(360deg) ;
     }
     100% {
@@ -365,9 +364,9 @@ export default {
 
   @keyframes letterE {
     0% {
-      transform:  translate(0px,0px)  rotate(90deg);
+      transform:  translate()  rotate(90deg);
     }
-    45% {
+    40%, 60%  {
       transform:  translate(-120px, 81px)  rotate(0deg);
     }
     100% {
@@ -377,9 +376,9 @@ export default {
 
   @keyframes letterR {
     0% {
-      transform:  translate(0px, 0px)  rotate(0deg);
+      transform:  translate()  rotate(0deg);
     }
-    41% {
+    40%, 60%  {
       transform:  translate(120px,-81px)  rotate(360deg) ;
     }
     100% {
@@ -388,9 +387,9 @@ export default {
   }
   @keyframes letterS {
     0% {
-      transform:  translate(0px,0px)  rotate(90deg);
+      transform:  translate()  rotate(90deg);
     }
-    55% {
+    40%, 60%  {
       transform:  translate(180px,-81px)  rotate(360deg) ;
     }
     100% {
@@ -400,9 +399,9 @@ export default {
 
   @keyframes letterO {
     0% {
-      transform:  translate(0px, 0px)  rotate(0deg);
+      transform:  translate()  rotate(0deg);
     }
-    52% {
+    40%, 60%  {
       transform:  translate(-32px, -101px)  rotate(360deg);
     }
     100% {
@@ -412,9 +411,9 @@ export default {
 
   @keyframes letterN {
     0% {
-      transform:  translate(0px, 0px)  rotate(0deg);
+      transform:  translate()  rotate(0deg);
     }
-    55% {
+    40%, 60% {
       transform:  translate(120px,-81px)  rotate(120deg) ;
     }
     100% {
@@ -424,9 +423,9 @@ export default {
 
   @keyframes letterA {
     0% {
-      transform:  translate(0px, 0px)  rotate(0deg);
+      transform:  translate()  rotate(0deg);
     }
-    62% {
+    40%, 60%  {
       transform:  translate(120px,-81px) rotate(359deg);
     }
     100% {
