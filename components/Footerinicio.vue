db24275129bad7b868e70bbbae03b7d15d74dd5f
Chaloulou75 <c.jeandey@gmail.com>
2021-06-08 11:06:01 -0300

    change email adress

--- components/Footerinicio.vue
+++ components/Footerinicio.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pt-2 mt-auto text-gray-800 bg-transparent">
    <div class="flex items-start w-full px-8 py-6">
-      <Mailto mail="personadisenio@gmail.com" target="_blank" class="hover:text-gray-600">
+      <Mailto mail="julietamercerat@gmail.com" target="_blank" class="hover:text-gray-600">
         <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
         </svg>
